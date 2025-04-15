5985ab487987265419b11f3e11c907b35a0f4e9c
Rafael Dadoorian <30996645+rdpreg@users.noreply.github.com>
2025-04-15 10:26:05 -0300

    Update simulador_completo_2.0.py

--- simulador_completo_2.0.py
+++ simulador_completo_2.0.py
@@ -82,7 +82,6 @@ if st.button("Simular Acúmulo"):
 
     fig.update_traces(
         name="Legenda",  # remove título solto
-        showlegend=False,  # <- garante que nada apareça no hover como título
         line=dict(color="green"),
         customdata=df[["Ano BR", "Patrimônio BR"]],
         hovertemplate="<b>Ano:</b> %{customdata[0]}<br><b>Patrimônio:</b> %{customdata[1]}<extra></extra>"
